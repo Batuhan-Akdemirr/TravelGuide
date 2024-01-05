2204b72eb43a2903e14185b9bf58308c2e4a1b37
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2024-01-05 19:51:31 +0300

    model created

---
+++ TravelGuide/TravelGuide/Models/Location.swift
@@ -0,0 +1,27 @@
+//
+//  Locations.swift
+//  TravelGuide
+//
+//  Created by Batuhan Akdemir on 5.01.2024.
+//
+
+import Foundation
+import MapKit
+
+
+struct Location: Codable, Identifiable , Equatable, Hashable{
+    let id: String
+    let name: String
+    let cityName: String
+    let description: String
+    let link: String
+    let latitude: Double
+    let longtitude: Double
+    let imageNames: [String]
+    
+    var coordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+    }
+    
+ 
+}
