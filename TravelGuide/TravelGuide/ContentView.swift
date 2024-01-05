75825686200e8c23cba337ced76367aa3570ce94
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2024-01-05 19:11:34 +0300

    all images added

---
+++ TravelGuide/TravelGuide/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TravelGuide
+//
+//  Created by Batuhan Akdemir on 5.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(.amsterdamRijksmuseum1)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelGuide/TravelGuide/TravelGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelGuideApp.swift
+//  TravelGuide
+//
+//  Created by Batuhan Akdemir on 5.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
